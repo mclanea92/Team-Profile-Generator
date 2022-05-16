c94a9e4177457d1524484739566ee6bc4a6fe544
Adam McLane <mclanea92@gmail.com>
2022-05-16 14:15:13 -0400

    fixing tests

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -4,18 +4,22 @@ const Engineer = require('../lib/Engineer');
 
 // creates object
 describe('create an engineer object', () => {
+    it('creates the engineer object from data', () => {
     const engineer = new Engineer('Adam', 100, 'mclanea92@gmail.com', 'mclanea92');
     expect(engineer.github).toEqual(expect.any(String));
-});
+})});
 
 // gets github info
 describe('get github from data of engineer', () => {
+    it('gets github from data of engineer', () => {
     const engineer = new Engineer('Adam', 100, 'mclanea92@gmail.com', 'mclanea92');
     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
-});
+})});
 
 describe('get role of employee', () => {
+    it('gets the role of employee from data', () => {
+    
     const engineer = new Engineer('Adam', 100, 'mclanea92@gmail.com', 'mclanea92');
 
     expect(engineer.getRole()).toEqual('Engineer');
-});
+})});
--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -2,18 +2,21 @@
 const Intern = require('../lib/Intern');
 
 describe('create a object', () => {
+    it('creates an object from the data', () => {
     const intern = new Intern('Adam', 100, 'mclanea92@gmail.com', 'UPenn');
     expect(intern.school).toEqual(expect.any(String));
-});
+})});
 
 // gets school from data
 describe('get school info', () => {
+    it('gets school info from intern data', () => {
     const intern = new Intern('Adam', 100, 'mclanea92@gmail.com', 'UPenn');
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
-});
+})});
 
 // gets role of intern
 describe('gets role of intern', () => {
+    it('gets the role of intern from data', () => {
     const intern = new Intern('Adam', 100, 'mclanea92@gmail.com', 'UPenn')
     expect(intern.getRole()).toEqual('Intern');
-});
+})});
